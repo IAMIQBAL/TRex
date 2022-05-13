6427d7990e25d24ea74a0bf52452d57cbb78dc0c
iamiqbal <muhammadiqbal2015@hotmail.com>
2022-05-13 19:51:33 +0500

    added argparse

--- Utility.py
+++ Utility.py
@@ -1,5 +1,7 @@
 from scapy.all import *
 from scapy.layers.inet import IP, ICMP
+import getopt
+import sys
 
 def OSdetect(ip):
     flag = -1
@@ -17,3 +19,51 @@ def OSdetect(ip):
                 print("OS Detection Failed")
 
     return flag
+
+def parser(argv):
+    help_arg = ""
+    network_arg = ""
+    user_arg = ""
+    ip_arg = ""
+
+    # Full Network scan = 0
+    # Network scan with OS detection = 1
+    # Ping Sweep only = 2
+    # Single ip OS detection = 3
+
+    help_arg = banner(argv)
+
+    try:
+        opts, args = getopt.getopt(argv[1:], "i:A:p:T:", ["network"])
+    except:
+        print(help_arg)
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print(help_arg)
+            sys.exit(2)
+        elif opt in ("-i"):
+            network_arg = arg
+            return 0, network_arg
+        elif opt in ("-A"):
+            network_arg = arg
+            return 1, network_arg
+        elif opt in ("-p"):
+            network_arg = arg
+            return 2, network_arg
+        elif opt in ("-T"):
+            ip_arg = arg
+            return 3, ip_arg
+
+def banner(argv):
+    return """Usage: {0} [OPTION...] [IP...]
+    Target Specification:
+    \tCan pass IP addresses, networks, etc.
+    \tEx: {0} -i 192.168.0.1/24
+    Complete network Scan:
+    \t{0} -i <network>
+    Network Scan with OS Detection:
+    \tsudo {0} -A <network>
+    Ping Sweep:
+    \t{0} -p <network>""".format(argv[0])
--- main.py
+++ main.py
@@ -1,29 +1,59 @@
 from PSweep import PSweep
 from Utility import OSdetect
+from Utility import parser
+from Utility import banner
 from PScan import PScan
 import queue
+import sys
 
 if __name__ == "__main__":
+    mode, nw = parser(sys.argv)
+
     q = queue.Queue()
     START = 0
-    END = 100
+    END = 65535
     THREADS = 100
     TIMEOUT = 0.1
-    pSweep = PSweep(q, '172.16.55.0/24', THREADS)
-    hosts, upHosts = pSweep.start()
-    print(f"Hosts Up: {hosts}")
-    print(f"Hosts: {upHosts}")
 
-    q = queue.Queue()
-    for i in upHosts:
-        ports = [i for i in range(START, END)]
-        scan = PScan(str(i), q, THREADS, TIMEOUT, ports)
-        scan.start()
+    print("NW: ", nw)
 
-    # OS Fingerprinting when host is UP
-    for i in upHosts:
-        res = OSdetect(str(i))
+    if mode == 0 or mode == 1:
+        START = int(input("Specify port range START: "))
+        END = int(input("Specify port range END: "))
+        THREADS = int(input("Enter number of THREADS: "))
+
+        pSweep = PSweep(q, nw, THREADS)
+        hosts, upHosts = pSweep.start()
+        print(f"Hosts Up: {hosts}")
+        print(f"Hosts: {upHosts}")
+
+        q = queue.Queue()
+
+        for i in upHosts:
+            ports = [i for i in range(START, END)]
+            scan = PScan(str(i), q, THREADS, TIMEOUT, ports)
+            scan.start()
+
+        if mode == 1:
+            # OS Fingerprinting when host is UP
+            for i in upHosts:
+                res = OSdetect(str(i))
+                if res == 0:
+                    print("Linux")
+                elif res == 1:
+                    print("Windows")
+    elif mode == 2:
+        THREADS = int(input("Enter number of THREADS: "))
+
+        pSweep = PSweep(q, nw, THREADS)
+        hosts, upHosts = pSweep.start()
+        print(f"Hosts Up: {hosts}")
+        print(f"Hosts: {upHosts}")
+    elif mode == 3:
+        res = OSdetect(str(nw))
         if res == 0:
             print("Linux")
         elif res == 1:
             print("Windows")
+    else:
+        banner(argv)
